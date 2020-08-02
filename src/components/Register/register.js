c8859d5a94359a05f989f1db7f27988e0fc2c7eb
sharvarikhedkar <anujakhedkar96@gmail.com>
2020-08-01 17:06:41 -0700

    header and registration form UI

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -1,12 +1,113 @@
 import React, {Component} from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './register.css';
 
 class Register extends Component{
     render(){
         return(
             <div className = "register">
-                <form>
-                    <p>Register Works!</p>
-                </form>
+                <div class="row">
+
+                    <div class="column-left"></div>
+
+                    <div class="column-center">
+
+                        <Form className = "form">
+                            
+                            <FormGroup>
+                            <Label id="labelforinput" for="firstName">Tell us a bit about yourself and your business loan needs</Label>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label id="labelforinput" for="firstName">First Name</Label>
+                                <Input type="tex" style={{ width: '30em' }}  name="firstName" id="firstName" placeholder="" required/>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label id="labelforinput" for="lastName">Last Name</Label>
+                                <Input type="tex" style={{ width: '30em' }}  name="lastName" id="lastName" placeholder="" required/>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label id="labelforinput" for="email">Best contact Email</Label>
+                                <Input type="email" style={{ width: '30em' }}  name="email" id="email" placeholder="" />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label id="labelforinput" for="phoneNumber">Mobile Phone Number (Accepted Format: 123-456-7890)</Label>
+                                <Input type="tel" style={{ width: '30em' }}  name="phoneNumber" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                required placeholder=""/>
+                            </FormGroup>
+
+                            <Label id="labelforinput" for="ownBusiness">Do you own a business?</Label>
+                            <div className ="radio-btn">
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="radio" name="radio1" />{' '}
+                                            Yes
+                                    </Label>
+                                </FormGroup>
+                                </div>
+                                <p></p>
+                                <div className ="radio-btn">
+                                <FormGroup check>   
+                                <Label check>
+                                    <Input type="radio" name="radio1" />{' '}
+                                            No
+                                </Label>
+                            </FormGroup>
+                            </div>
+
+                            <FormGroup>
+                                <Label id="labelforinput" for="businessName">Business Name</Label>
+                                <Input type="tex" style={{ width: '30em' }}  name="businessName" id="businessName" placeholder="" required/>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label id="labelforinput" for="loanAmout">Desired Loan Amount</Label>
+                                <input type="number" style={{ width: '30em' }} class="form-control currency" placeholder="$" name="amount" min="1" maxlength="12" />
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label id="labelforinput" for="loanPurpose">Purpose of Loan</Label>
+                                <Input type="select" style={{ width: '30em' }}  name="loanPurpose" id="loanPurpose">
+                                <option>Working Capital</option>
+                                <option>Growth</option>
+                                <option>Inventory</option>
+                                <option>Equipment</option>
+                                <option>Startup</option>
+                                <option>Debt Refinance</option>
+                                <option>A/R Finance</option>
+                                <option>Acquisition/Buyout Capital</option>
+                                <option>Other</option>
+                                </Input>
+                            </FormGroup>
+
+                            <Label id="labelforinput" for="beenBusiness">Have you been in business for at least 9 months?</Label>
+                            <div className ="radio-btn">
+                                <FormGroup check>
+                                <Label check>
+                                    <Input type="radio" name="radio1" />{' '}
+                                            Yes
+                                    </Label>
+                                </FormGroup>
+                                </div>
+                                <p></p>
+                                <div className ="radio-btn">
+                                <FormGroup check>
+                                <Label check>
+                                    <Input type="radio" name="radio1" />{' '}
+                                            No
+                                    </Label>
+                                </FormGroup>
+                            </div>
+                            <br />
+                            <Button color="primary" style={{width : '480px'}}size="lg" block>Next</Button>
+                        </Form>
+                        </div>
+                    <div class="column-right"></div>
+                    </div>
             </div>
         )
     }
--- src/components/shared/Header/header.component.js
+++ src/components/shared/Header/header.component.js
@@ -4,10 +4,15 @@ import './header.css';
 
 function Header(props){
     return (
-        <div className = "header">
-            <img className="logo" style ={{height: '50px', width: '150px', align:'left'}}src = {Logo} alt = 'logo'>
-            </img>
-               
+        <div style={{width:'100%'}} className = "header">
+            <div  style={{width:'95%', float:'left'}}> 
+                <img className="logo" style ={{height: '50px', width: '150px', float:'left'}}src = {Logo} alt = 'logo'>
+                </img>
+            </div>
+
+            <div>
+                <button className="language" style={{float:'right'}}>ESPAÑOL</button>
+            </div>  
         </div>
     )
 }
